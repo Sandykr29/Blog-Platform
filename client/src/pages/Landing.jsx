f3ad6a4edb81f920766be6bd9a67e8a6b742082c
sandykr29 <sandeepkumar29774@yahoo.com>
2025-04-28 17:13:23 +0530

    updated

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -25,11 +25,9 @@ const LandingPage = () => {
 
       {/* Section with Background Image */}
       <div
-        className="relative w-full max-w-screen-lg mx-auto px-5 py-20 space-y-10"
+        className="relative w-full max-w-screen-lg mx-auto px-5 py-20 space-y-10 bg-cover bg-center"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1512757776214-26d36777b513?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
         }}
       >
         <div className="bg-black bg-opacity-40 p-10 rounded-lg">
@@ -69,6 +67,34 @@ const LandingPage = () => {
         <LandingSection
           image="https://plus.unsplash.com/premium_photo-1664368832311-7fe635e32c7c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhdmVsfGVufDB8fDB8fHww"
           text="Write not because you have to, but because you *can't not*."
+          flip={true}
+        />
+
+        {/* Third Landing Section with a map, camera, pencil, books, and bag */}
+        <LandingSection
+          image="https://images.unsplash.com/photo-1488646953014-85cb44e25828?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          text="Discover the world through creativity and exploration."
+          flip={false}
+        />
+
+        {/* Fourth Landing Section with Eiffel Tower */}
+        <LandingSection
+          image="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          text="Experience the beauty of landmarks around the world."
+          flip={true}
+        />
+
+        {/* Fifth Landing Section with a girl in a pool */}
+        <LandingSection
+          image="https://images.unsplash.com/photo-1532347922424-c652d9b7208e?q=80&w=1372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          text="Relax and unwind, feel the calmness in every moment."
+          flip={false}
+        />
+
+        {/* Sixth Landing Section with an airplane over snow */}
+        <LandingSection
+          image="https://plus.unsplash.com/premium_photo-1679830513922-d11c0fedfa99?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjU5fHx0cmF2ZWx8ZW58MHx8MHx8fDA%3D"
+          text="Soar high above the clouds, where adventures await."
           flip={true}
         />
       </div>
